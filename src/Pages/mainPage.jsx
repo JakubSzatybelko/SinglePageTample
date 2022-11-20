18f916db0920fa84a61aeb78f6f08ae39bd59789
JakSzatan <ddt1@op.pl>
2022-11-20 14:00:45 +0100

    kamil gill setup

--- src/Pages/mainPage.jsx
+++ src/Pages/mainPage.jsx
@@ -6,125 +6,94 @@ export default class MainPage extends React.Component {
       <div>
         <div className="container mt-5">
           <div data-aos="fade-up" className="mb-5">
-            <h2>News</h2>
-            Jakieś Newsy
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-              tempus tempus sapien, placerat mattis lacus maximus eu. Donec
-              sollicitudin leo auctor metus suscipit aliquet. Sed eget commodo
-              est. Vestibulum dapibus nunc ut turpis suscipit placerat. Ut
-              euismod mauris a odio lobortis, id consectetur massa tincidunt. Ut
-              ut luctus libero. Quisque a luctus lacus, et convallis urna. Etiam
-              cursus, mi eu aliquam mattis, turpis mauris sagittis orci, id
-              aliquet odio tortor vel diam. Sed vitae metus nec enim facilisis
-              tristique. Aliquam pulvinar enim sodales diam tincidunt faucibus.
-              Morbi quis lectus lorem. Fusce eleifend mauris a odio lobortis,
-              non venenatis justo maximus.
-            </p>
+            <h2 className="text-dark">
+              KOMPLEKSOWE USŁUGI REMONTOWO-BUDOWLANE I ELEWACYJNE, KAMIL GILL
+            </h2>
             <hr />
-          </div>
-          <div data-aos="fade-up" className="mb-5">
-            <h2 id="Ceny">Ceny</h2>
-            Tutaj pojawi się cennik
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-              tempus tempus sapien, placerat mattis lacus maximus eu. Donec
-              sollicitudin leo auctor metus suscipit aliquet. Sed eget commodo
-              est. Vestibulum dapibus nunc ut turpis suscipit placerat. Ut
-              euismod mauris a odio lobortis, id consectetur massa tincidunt. Ut
-              ut luctus libero. Quisque a luctus lacus, et convallis urna. Etiam
-              cursus, mi eu aliquam mattis, turpis mauris sagittis orci, id
-              aliquet odio tortor vel diam. Sed vitae metus nec enim facilisis
-              tristique. Aliquam pulvinar enim sodales diam tincidunt faucibus.
-              Morbi quis lectus lorem. Fusce eleifend mauris a odio lobortis,
-              non venenatis justo maximus.
-            </p>
-            <hr />
-          </div>
-          <div data-aos="fade-up" className="mb-5">
-            <h2 id="About">Info</h2>
-            <h4 className="ps-5">Info o studiu</h4>
-            <p className="overflow-auto">
+            <div className="d-lg-flex">
+              <p className="fs-5">
+                Doskonale rozumiemy potrzeby współczesnych konsumentów oraz ich
+                oczekiwania w zakresie prac dotyczących metamorfozy wnętrz.
+                Działamy na rynku usług budowlanych i remontowych od dawna w tym
+                czasie pod naszym profesjonalnym okiem dokonano na terenie
+                Warmii i Mazur oraz okolic wielu udanych modernizacji. Dzięki
+                zdobytemu doświadczeniu i bogatej wiedzy współpracujących z
+                firmą fachowców możemy zapewnić profesjonalne usługi remontowe
+                zarówno mieszkań i domów prywatnych, jak i lokali użytkowych,
+                sklepów, biur, miejsc publicznych oraz elewacji i dachów.
+                Towarzyszymy Państwu na każdym etapie projektu, począwszy od
+                stworzenia dopasowanego do indywidualnych potrzeb planu
+                architektonicznego, poprzez zakup najlepszej jakości materiałów,
+                aż do terminowej i co najważniejsze, rzetelnej realizacji
+                usługi.
+              </p>
               <img
-                data-aos="slide-right"
-                style={{ height: "200px" }}
-                className="float-start rounded me-3"
-                src="https://i.postimg.cc/NfvZwj12/244622249-866020767609897-3784407638343700036-n.jpg"
+                data-aos="slide-left"
+                style={{ height: "500px" }}
+                className="rounded ms-auto me-3 img-fluid"
+                src="P1.jpg"
               />
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-              tempus tempus sapien, placerat mattis lacus maximus eu. Donec
-              sollicitudin leo auctor metus suscipit aliquet. Sed eget commodo
-              est. Vestibulum dapibus nunc ut turpis suscipit placerat. Ut
-              euismod mauris a odio lobortis, id consectetur massa tincidunt. Ut
-              ut luctus libero. Quisque a luctus lacus, et convallis urna. Etiam
-              cursus, mi eu aliquam mattis, turpis mauris sagittis orci, id
-              aliquet odio tortor vel diam. Sed vitae metus nec enim facilisis
-              tristique. Aliquam pulvinar enim sodales diam tincidunt faucibus.
-              Morbi quis lectus lorem. Fusce eleifend mauris a odio lobortis,
-              non venenatis justo maximus.
-            </p>
-
-            <hr />
+            </div>
           </div>
           <div data-aos="fade-up" className="mb-5 overflow-hidden">
             <h2 id="About">Lokalizacja</h2>
             <div className="d-lg-flex">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-                tempus tempus sapien, placerat mattis lacus maximus eu. Donec
-                sollicitudin leo auctor metus suscipit aliquet. Sed eget commodo
-                est. Vestibulum dapibus nunc ut turpis suscipit placerat. Ut
-                euismod mauris a odio lobortis, id consectetur massa tincidunt.
-                Ut ut luctus libero. Quisque a luctus lacus, et convallis urna.
-                Etiam cursus, mi eu aliquam mattis, turpis mauris sagittis orci,
-                id aliquet odio tortor vel diam. Sed vitae metus nec enim
-                facilisis tristique. Aliquam pulvinar enim sodales diam
-                tincidunt faucibus. Morbi quis lectus lorem. Fusce eleifend
-                mauris a odio lobortis, non venenatis justo maximus.aliquet odio
-                tortor vel diam. Sed vitae metus nec enim facilisis tristique.
-                Aliquam pulvinar enim sodales diam tincidunt faucibus. Morbi
-                quis lectus lorem. Fusce eleifend mauris a odio lobortis, non
-                venenatis justo maximus.
-              </p>
               <img
-                data-aos="slide-left"
-                style={{ "max-height": "500px" }}
+                data-aos="slide-right"
+                style={{ "max-height": "800px" }}
                 className="rounded ms-auto me-3 img-fluid"
-                src="https://www.seashepherd.tattoo/wp-content/uploads/2019/10/Studiophoto-Homepage.jpg"
+                src="P3.jpg"
               />
+              <p className="fs-5">
+                Przeprowadzamy kompleksowe remonty całych budynków lub wybranych
+                przez Państwa pomieszczeń: salonów, kuchni oraz łazienek.
+                Działamy zarówno w Morągu jak i okolicznych miastach województwa
+                Warmińsko-Mazurksiego. Gwarantujemy pełne zaangażowanie w każde
+                zlecenie oraz spektakularny efekt końcowy. Zajmiemy się
+                malowaniem, flizowaniem czy renowacją podłóg, pracami
+                elewacyjnymi, remontami dachów, łazienek, pomieszczeń użytkowych
+                etc. Zmodernizujemy stare instalacje i zbudujemy nowoczesne
+                pomieszczenia.
+              </p>
             </div>
-
-            <hr />
+          </div>
+          <div data-aos="fade-up" className="mb-5 d-lg-flex">
+            <div>
+              <p className="fs-5">
+                Doradzimy w kwestiach kolorystyki, układu oraz wyboru
+                poszczególnych elementów, tak aby wykonany przez nas remont
+                wnętrza lub elewacji budynku był jak najbliższy Państwa
+                wyobrażeniom.
+              </p>
+              <p className="fs-5">
+                Świadczymy usługi remontowe na terenie Morąga i okolic.
+              </p>
+            </div>
+            <img
+              data-aos="slide-left"
+              style={{ height: "400px" }}
+              className="rounded me-3"
+              src="P2.jpg"
+            />
           </div>
         </div>
-        <div data-aos="fade" className="container mb-4">
+        <div id="kotakt" data-aos="fade" className="container mb-4">
           <h2>Kontakt</h2>
           <hr />
-          <div>
-            Mochnackiego 3/5 Olsztyn, 10-036 Polska
+          <div className="d-flex">
+            <i className="bi bi-geo-alt text-primary me-2 fs-5"></i>
             <span>
-              <a href="">
-                <i className="bi bi-geo-alt text-primary ms-1 fs-5"></i>
-              </a>
+              <div>Usługi remontowo-budowlane Kamil Gill</div>
+              <div>Słonecznik 14-300 Morąg</div>
             </span>
           </div>
           <span>
-            <a href="">
-              <i className="bi bi-facebook text-primary me-2 fs-5"></i>
-            </a>
+            <i className="bi bi-envelope me-2 fs-5 text-primary"></i>
+            gill.kamil83@gmail.com
           </span>
-          <span>
-            <a href="" style={{ color: "#e95950" }}>
-              <i className="bi bi-instagram me-2 fs-5"></i>
-            </a>
-          </span>
-          <span>
-            <a href="">
-              <i className="bi bi-envelope me-2 fs-5"></i>
-            </a>
-          </span>
-          <div style={{ color: "#fccc63" }}>
-            <i className="bi bi-telephone me-2 fs-5"></i>XXX 201 222
+          <div>
+            <i className="bi bi-telephone text-primary me-2 fs-5"></i>+48 696
+            286 009
           </div>
         </div>
       </div>
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -4,15 +4,20 @@ import { Link } from "react-router-dom";
 export default class Navbar extends React.Component {
   render() {
     return (
-<div className='hero'>
-  <div className='menu-wrapper'>
-    <div className='nav-litem'>showcase</div>
-    <div className='nav-litem'>contact</div>
-  </div>
-  <div className="hero-one"></div>
-  <div className="hero-two"></div>
-  <div className="header-title"><div className="header-primary">Usługi remontowo Budowlane</div><span className="header-sub">Kamil Gill</span></div>
-</div>
+      <div className="hero">
+        <div className="menu-wrapper">
+          <div className="nav-litem">
+            <a href="#kotakt">Kontakt</a>
+          </div>
+        </div>
+        <div className="hero-one"></div>
+        {/* <div className="hero-two"></div> */}
+        <div className="header-title">
+          <div className="header-primary">Usługi remontowo Budowlane</div>
+          <hr className="divider" />
+          <span className="header-sub">Kamil Gill</span>
+        </div>
+      </div>
     );
   }
 }
